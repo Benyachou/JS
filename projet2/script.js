06220b7ec99096cdb43fc55ff8bd4587a29ffa01
Yas <ben.yachou@yahoo.com>
2022-09-09 13:23:09 +0200

    Projet 3

---
+++ projet2/script.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector('.btn');
+const modal = document.querySelector('.modal');
+
+let toggle = false;
+
+btn.addEventListener('click', () => {
+
+    toggle = !toggle;
+
+    if (toggle) {
+        modal.style.display = 'block';
+
+        setTimeout (() => {
+
+            modal.style.opacity = 1;
+        }, 10)
+    }
+    else if (toggle === false){
+
+        modal.style.display = "none";
+
+        setTimeout ( ()=>  {
+            modal.style.display = "none";
+        }, 10)
+    }
+
+    
+})
