da62489798f3ce99c50d9ece3e3d858ca04c590d
Yas <ben.yachou@yahoo.com>
2022-09-09 13:13:49 +0200

    premier projet

---
+++ projet0/app.js
@@ -0,0 +1,20 @@
+// const premierTitre = document.querySelector('.premier-titre');
+// const premierID = document.getElementById('premier-id');
+// const titreNu = document.querySelector('h3')
+// const allItems = document.querySelectorAll('.premier-titre')
+// const liste = document.querySelector('ul')
+// const i2 = document.querySelector('.i2')
+
+
+// premierTitre.innerText = 'Hello world'
+
+// const parent = document.querySelector('.parent');
+
+// parent.innerHTML = '<h1>En texte</h1>'
+
+// let nvEl = document.createElement('li');
+// nvEl.innerText = "Nouvel item !";
+
+// liste.appendChild(nvEl)
+
+// liste.children[1].replaceWith(nvEl)
